f9faa886bb842430239dbc63669d1106e7ae18cb
Fanny Leon <tonyleor@gmail.com>
2025-05-02 14:51:32 +0200

    poop!!!!!

--- Gloomberts Revenge Unity Proj/Assets/Scripts/UI Manager.cs
+++ Gloomberts Revenge Unity Proj/Assets/Scripts/UI Manager.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameObject uiSpace;
+    public GameObject keyIconPrefab;
+    public Image keyUnlocked;
+    public Image keyLocked;
+    public Vector3 greenIconPositon;
+    public Vector3 yellowIconPosition;
+    public Vector3 blueIconPositon;
+    public Vector3 purpleIconPosition;
+    public Vector3 cyanIconPosition;
+    public Vector3 redIconPosiiton;
+    public Vector3 blackIconPosition;
     void Start()
     {
-        
+        uiSpace = GetComponent<Canvas>().gameObject;
     }
 
     // Update is called once per frame
